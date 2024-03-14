ca910f90fca11107da1cf4f1bed25f1aa3acf566
albanovs <albvnovs@gmail.com>
2024-03-15 02:54:04 +0600

    register

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import SignUp from "./pages/auth/sign-up/sign-up";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './App.css'
+import { useEffect } from "react";
 
 function App() {
   const location = useLocation();
+
+  const handleScroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    handleScroll();
+  }, [location]);
+  
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/" element={<SignIn />} />
-        <Route path="/send-code" element={<EmailCode />} />
-      </Routes> */}
       <TransitionGroup>
         <CSSTransition key={location.key} classNames="fade" timeout={300}>
           <Routes location={location}>
