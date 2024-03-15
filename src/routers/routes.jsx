5feb814e6c649b6cf7e04527628adb7b487590cc
albanovs <albvnovs@gmail.com>
2024-03-15 12:01:58 +0600

    prevews 2

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -8,27 +8,27 @@ const routes = [
     {
         path: '/',
         name: 'Главная',
-        icon: <IoHome size={30} />
+        icon: <IoHome size={20} />
     },
     {
         path: '/payments',
         name: 'Платежи',
-        icon: <BsArrowUpLeftCircle size={30} />
+        icon: <BsArrowUpLeftCircle size={20} />
     },
     {
         path: '/discount',
         name: 'Скидки',
-        icon: <BiSolidDiscount size={30} />
+        icon: <BiSolidDiscount size={20} />
     },
     {
         path: '/market',
         name: 'Маркет',
-        icon: <FiShoppingBag size={30} />
+        icon: <FiShoppingBag size={20} />
     },
     {
         path: '/profile',
         name: 'Профиль',
-        icon: <MdAccountCircle size={30} />
+        icon: <MdAccountCircle size={20} />
     },
 ]
 
