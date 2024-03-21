416e20d84602a4e0513d3df9dc89bfe25eebd634
albanovs <albvnovs@gmail.com>
2024-03-21 18:52:46 +0600

    history

--- src/pages/payments/payments.jsx
+++ src/pages/payments/payments.jsx
@@ -5,9 +5,11 @@ import { MdOutlineHistory } from "react-icons/md";
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 import { MdCancel } from "react-icons/md";
 import { MdOutlineNavigateNext } from "react-icons/md";
+import Modal from '../../containers/UI/Modal/Modal';
 export default function Payments() {
 
     const [history, setHistory] = useState([])
+    const [modal, setModal] = useState(false)
 
     useEffect(() => {
         const fetchDataHistory = async () => {
@@ -26,11 +28,15 @@ export default function Payments() {
         fetchDataHistory()
     }, [])
 
-
     return (
         <div>
             <div className='section-payments'>
-                <div className='button_period'>Выбрать период <MdOutlineNavigateNext /> </div>
+                <div className='button_period' onClick={() => setModal(!modal)}>Выбрать период <MdOutlineNavigateNext /> </div>
+                {modal && (
+                    <Modal setIsModalOpen={setModal}>
+
+                    </Modal>
+                )}
                 {
                     history.map((data, index) => {
 
@@ -49,7 +55,10 @@ export default function Payments() {
                                                         : data.status === 1
                                                             ? { color: '#00FF7F' }
                                                             : { color: 'var(--orange)' }}
-                                                >{data.total} <span>{data.status === 0 ? <MdCancel /> : data.status === 1 ? <IoCheckmarkDoneCircleSharp /> : <MdOutlineHistory />}</span></p>
+                                                > {data.info === "Покупка Asman" && data.status === 2 ?
+                                                    <span style={{ fontSize: "12px" }}>В обработке</span>
+                                                    : data.info === "Покупка Asman" && data.status === 0 ? <span style={{ fontSize: "12px" }}>Отклонено</span> : data.type === 1 ? `+${data.total}` : `-${data.total}`}
+                                                    <span>{data.status === 0 ? <MdCancel /> : data.status === 1 ? <IoCheckmarkDoneCircleSharp /> : <MdOutlineHistory />}</span></p>
                                             </div>
                                         )
                                     })
