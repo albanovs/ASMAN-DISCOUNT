788f0d613489ee28505ebe0e9f12954ed3549f41
beka serkan <bekaserkan@bekas-MacBook-Pro.local>
2024-03-22 18:34:06 +0600

    change

--- src/pages/discount/discount.jsx
+++ src/pages/discount/discount.jsx
@@ -3,17 +3,46 @@ import "./discount.css";
 import Header from "../../containers/header/header";
 import { api } from "../../Api";
 import { useNavigate } from "react-router-dom";
+import cate1 from "../../views/disc/one.svg";
+import cate2 from "../../views/disc/two.svg";
+import cate3 from "../../views/disc/three.svg";
+import cate4 from "../../views/disc/four.svg";
+import cate5 from "../../views/disc/five.svg";
+import { useSelector } from "react-redux";
 
 export default function Discount() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const names = useSelector((state) => state.user_info.user_info);
+  const [cate, setCate] = useState("");
+  const [list, setList] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
+    if (names) {
+      setCate(names.status);
+    }
+  }, [names]);
+
+  useEffect(() => {
+    if (cate) {
+      if (list) {
+        function searchArray(list, keyword) {
+          return list
+            .filter((item) => item.status.includes(keyword))
+            .map((item) => item);
+        }
+        const arrayData = searchArray(list, cate);
+        setData(arrayData[0]?.categories);
+      }
+    }
+  }, [list, cate]);
+
+  useEffect(() => {
     api
-      .get("/discount/list")
+      .get("/discount/list/")
       .then((response) => {
-        setData(response.data);
+        setList(response.data);
         setTimeout(() => {
           setLoading(false);
         }, 300);
@@ -24,13 +53,51 @@ export default function Discount() {
           setLoading(false);
         }, 300);
       });
-  }, []);
+  }, [cate]);
 
   return (
     <div className="discount">
       <Header />
       <div className="container_2">
-        <h1 className="title">Скидки</h1>
+        <div>
+          <h1 className="title">Скидки</h1>
+          <p className="text_dis">
+            Нажмите на статус, чтобы узнать, на какие фитнес-центры или
+            рестораны есть скидки.
+          </p>
+        </div>
+        <div className="cateing">
+          <div
+            onClick={() => setCate("Стандарт")}
+            className={`cate ${cate === "Стандарт" && "active"}`}
+          >
+            <img src={cate1} alt="" />
+          </div>
+          <div
+            onClick={() => setCate("Бронза")}
+            className={`cate ${cate === "Бронза" && "active"}`}
+          >
+            <img src={cate2} alt="" />
+          </div>
+          <div
+            onClick={() => setCate("Серебро")}
+            className={`cate ${cate === "Серебро" && "active"}`}
+          >
+            <img src={cate3} alt="" />
+          </div>
+          <div
+            onClick={() => setCate("Золото")}
+            className={`cate ${cate === "Золото" && "active"}`}
+          >
+            <img src={cate4} alt="" />
+          </div>
+          <div
+            onClick={() => setCate("VIP")}
+            className={`cate ${cate === "VIP" && "active"}`}
+          >
+            <img src={cate5} alt="" />
+          </div>
+        </div>
         {loading ? (
           <>
             <div>
@@ -55,8 +122,8 @@ export default function Discount() {
             </div>
           </>
         ) : (
-          data.map((el) => (
-            <div>
+          data.map((el, key) => (
+            <div key={key}>
               <h2>{el.name}:</h2>
               <div className="discount_block">
                 {el.partners.map((item) => (
