58240551345231224252b235952910767cdda1be
beka serkan <bekaserkan@bekas-MacBook-Pro.local>
2024-04-01 21:54:52 +0600

    change

--- src/pages/payments/components/waiting/waitng-process.jsx
+++ src/pages/payments/components/waiting/waitng-process.jsx
@@ -13,7 +13,7 @@ export default function WaitingProcess() {
     const navigate = useNavigate()
     return (
         <div className='waiting-contain'>
-            <div><FiChevronLeft onClick={() => navigate('/payments')} size={40} /></div>
+            <div><FiChevronLeft onClick={() => navigate('/')} size={40} /></div>
             <BsClockFill size={50} color='#fdb602' />
             <h1>Платеж в обработке</h1>
             <h2>Проверка реквизитов займет <br /> от 1 минуты до 24 часа</h2>
